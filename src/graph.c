c5da5ef93c4e7e22c00a45b3df58c9532c556574
jmimu <jmimu@free.fr>
2016-07-16 16:44:13 +0200

    graph multi-thread

--- src/graph.c
+++ src/graph.c
@@ -1,58 +1,66 @@
 #include "graph.h"
 
 #include <stdio.h>
+#include <omp.h>
 
 #include "dbg.h"
 
 struct Graph graph;
+int nb_threads=-1;
 
 bool graph_init(int _window_w,int _window_h,
 				int _render_w,int _render_h,const char* title)
 {
-  graph.window_w=_window_w;
-  graph.window_h=_window_h;
-  graph.render_w=_render_w;
-  graph.render_h=_render_h;
-  if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-  {
-	printf("SDL2 could not initialize! SDL2_Error: %s\n",SDL_GetError());
-	return false;
-  }
-  graph.window = SDL_CreateWindow(title,SDL_WINDOWPOS_CENTERED,
+	nb_threads=omp_get_max_threads();
+	graph.window_w=_window_w;
+	graph.window_h=_window_h;
+	graph.render_w=_render_w;
+	graph.render_h=_render_h;
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf("SDL2 could not initialize! SDL2_Error: %s\n",SDL_GetError());
+		return false;
+	}
+	graph.window = SDL_CreateWindow(title,SDL_WINDOWPOS_CENTERED,
 				SDL_WINDOWPOS_CENTERED,graph.window_w,graph.window_h,
 				SDL_WINDOW_SHOWN);//SDL_WINDOW_FULLSCREEN_DESKTOP
-  //if SDL_WINDOW_FULLSCREEN_DESKTOP
-  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");  // make the scaled rendering look smoother.
-  //SDL_RenderSetLogicalSize(sdlRenderer, 640, 480);
+	//if SDL_WINDOW_FULLSCREEN_DESKTOP
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");  // make the scaled rendering look smoother.
+	//SDL_RenderSetLogicalSize(sdlRenderer, 640, 480);
 
-  graph.renderer = SDL_CreateRenderer(graph.window, -1, 0);
+	graph.renderer = SDL_CreateRenderer(graph.window, -1, 0);
 
- 
-  graph.texture = SDL_CreateTexture(graph.renderer,
-							   SDL_PIXELFORMAT_ARGB8888,
-							   SDL_TEXTUREACCESS_STREAMING,
-							   graph.render_w, graph.render_h);
-  graph.myPixels=(uint32_t*) malloc (graph.render_w*graph.render_h*sizeof(uint32_t));
-  return true;
+
+	graph.texture = SDL_CreateTexture(graph.renderer,
+						   SDL_PIXELFORMAT_ARGB8888,
+						   SDL_TEXTUREACCESS_STREAMING,
+						   graph.render_w, graph.render_h);
+	graph.myPixels=(uint32_t*) malloc (graph.render_w*graph.render_h*sizeof(uint32_t));
+	graph.threadColPixels=(uint32_t**) malloc (nb_threads*sizeof(uint32_t*));
+	for (int i=0;i<nb_threads;i++)
+	{
+		graph.threadColPixels[i]=(uint32_t*) malloc (graph.render_h*sizeof(uint32_t));
+	}
+	return true;
 }
 
 
 
 void graph_start_frame()
 {
-  for (int i=0;i<graph.render_w*graph.render_h;i++)
-	graph.myPixels[i]=0x00;
+	for (int i=0;i<graph.render_w*graph.render_h;i++)
+		graph.myPixels[i]=0x00;
 #ifdef DBG_GRAPH
-	graph_vline(graph.render_w/2,0,graph.render_h,0xFF00B040);
+		graph_vline(graph.render_w/2,0,graph.render_h,0xFF00B040);
 #endif
 }
 
 void graph_end_frame()
 {
-  SDL_UpdateTexture(graph.texture, NULL, graph.myPixels, graph.render_w * sizeof (uint32_t));
-  //SDL_RenderClear(renderer);
-  SDL_RenderCopy(graph.renderer, graph.texture, NULL, NULL);
-  SDL_RenderPresent(graph.renderer);
+	SDL_UpdateTexture(graph.texture, NULL, graph.myPixels, graph.render_w * sizeof (uint32_t));
+	//SDL_RenderClear(renderer);
+	SDL_RenderCopy(graph.renderer, graph.texture, NULL, NULL);
+	SDL_RenderPresent(graph.renderer);
 }
 
 
@@ -88,15 +96,49 @@ void graph_vline(int x,int y1,int y2,uint32_t rgba)
 	}
 }
 
+void graph_vline_threadCol(int thread,int y1,int y2,uint32_t rgba)
+{	int ymin;
+	int ymax;
+	if (y1>y2)
+	{
+		ymin=graph.render_h-1-y1;
+		ymax=graph.render_h-1-y2;
+	}else{
+		ymin=graph.render_h-1-y2;
+		ymax=graph.render_h-1-y1;
+	}
+	if (ymin<0) ymin=0;
+	if (ymax>=graph.render_h) ymax=graph.render_h-1;
+	
+	for (int y=ymin+1;y<=ymax;y++)
+		graph.threadColPixels[thread][y]=rgba;
+}
+
+void graph_clear_threadCol(int thread)
+{
+	memset (graph.threadColPixels[thread], 0, graph.render_h*4 );
+}
+
+void graph_write_threadCol(int thread, int x)
+{
+	unsigned int i=x;
+	for (int y=0;y<=graph.render_h;y++)
+	{
+		graph.myPixels[i]=graph.threadColPixels[thread][y];
+		i+=graph.render_w;
+	}
+}
 
 void graph_close()
 {
-  SDL_DestroyTexture(graph.texture);
-  SDL_DestroyRenderer(graph.renderer);
-  SDL_DestroyWindow(graph.window);
-  
-  free(graph.myPixels);
-  SDL_Quit();
+	SDL_DestroyTexture(graph.texture);
+	SDL_DestroyRenderer(graph.renderer);
+	SDL_DestroyWindow(graph.window);
+	for (int i=0;i<nb_threads;i++)
+		free(graph.threadColPixels[i]);
+	free(graph.threadColPixels);  
+	free(graph.myPixels);
+	SDL_Quit();
 }
 
 
--- src/graph.h
+++ src/graph.h
@@ -21,9 +21,11 @@ struct Graph{
 		SDL_Renderer *renderer;
 		SDL_Texture *texture;		
 		uint32_t *myPixels;
+		uint32_t **threadColPixels;//one column for one thread
 };
 
 extern struct Graph graph;
+extern int nb_threads;
 
 bool graph_init(int _window_w,int _window_h,
 				int _render_w,int _render_h,const char* title);
@@ -31,6 +33,9 @@ bool graph_init(int _window_w,int _window_h,
 void graph_putpixel_rgb(int x,int y,uint8_t r,uint8_t g,uint8_t b);//todo: add z!
 void graph_putpixel(int x,int y,uint32_t rgba);//todo: add z!
 void graph_vline(int x,int y1,int y2,uint32_t rgba);
+void graph_vline_threadCol(int thread,int y1,int y2,uint32_t rgba);
+void graph_clear_threadCol(int thread);
+void graph_write_threadCol(int thread, int x);
 void graph_close();
 void graph_start_frame();
 void graph_end_frame();
